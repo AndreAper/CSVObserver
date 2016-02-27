a168f97bcb947c43991575fb025cb486cd694f36
Andre Aper <andre.aper@outlook.com>
2016-02-27 15:25:59 +0100

    Upload Files

---
+++ CSVObserver/frmMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CSVObserver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.lblCurrentFile = new System.Windows.Forms.Label();
+            this.chkBxFswEnable = new System.Windows.Forms.CheckBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.splitContainer.Panel1.Controls.Add(this.lblCurrentFile);
+            this.splitContainer.Panel1.Controls.Add(this.chkBxFswEnable);
+            this.splitContainer.Panel1.Controls.Add(this.btnOpenFile);
+            this.splitContainer.Panel1MinSize = 32;
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer.Size = new System.Drawing.Size(1081, 460);
+            this.splitContainer.SplitterDistance = 32;
+            this.splitContainer.SplitterWidth = 1;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // lblCurrentFile
+            // 
+            this.lblCurrentFile.AutoSize = true;
+            this.lblCurrentFile.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentFile.ForeColor = System.Drawing.Color.White;
+            this.lblCurrentFile.Location = new System.Drawing.Point(215, 11);
+            this.lblCurrentFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCurrentFile.Name = "lblCurrentFile";
+            this.lblCurrentFile.Size = new System.Drawing.Size(28, 15);
+            this.lblCurrentFile.TabIndex = 2;
+            this.lblCurrentFile.Text = "File:";
+            // 
+            // chkBxFswEnable
+            // 
+            this.chkBxFswEnable.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkBxFswEnable.Enabled = false;
+            this.chkBxFswEnable.FlatAppearance.BorderSize = 0;
+            this.chkBxFswEnable.FlatAppearance.CheckedBackColor = System.Drawing.Color.ForestGreen;
+            this.chkBxFswEnable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chkBxFswEnable.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBxFswEnable.ForeColor = System.Drawing.Color.White;
+            this.chkBxFswEnable.Location = new System.Drawing.Point(112, 4);
+            this.chkBxFswEnable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkBxFswEnable.Name = "chkBxFswEnable";
+            this.chkBxFswEnable.Size = new System.Drawing.Size(95, 32);
+            this.chkBxFswEnable.TabIndex = 1;
+            this.chkBxFswEnable.Text = "OFFLINE";
+            this.chkBxFswEnable.UseVisualStyleBackColor = true;
+            this.chkBxFswEnable.CheckedChanged += new System.EventHandler(this.chkBxFswEnable_CheckedChanged);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.FlatAppearance.BorderSize = 0;
+            this.btnOpenFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFile.Font = new System.Drawing.Font("Calibri Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.ForeColor = System.Drawing.Color.White;
+            this.btnOpenFile.Location = new System.Drawing.Point(4, 4);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(100, 32);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open file";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1081, 427);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fileSystemWatcher
+            // 
+            this.fileSystemWatcher.EnableRaisingEvents = true;
+            this.fileSystemWatcher.SynchronizingObject = this;
+            this.fileSystemWatcher.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher_Changed);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 460);
+            this.Controls.Add(this.splitContainer);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.Text = "CSVObserver";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox chkBxFswEnable;
+        private System.IO.FileSystemWatcher fileSystemWatcher;
+        private System.Windows.Forms.Label lblCurrentFile;
+    }
+}
+
---
+++ CSVObserver/frmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace CSVObserver
+{
+    public partial class frmMain : Form
+    {
+        FileInfo file;
+        string[] content;
+
+        void UpdateDGV() 
+        {
+            if (file.Exists)
+            {
+                //Alle Zeilen
+                content = File.ReadAllLines(file.FullName);
+
+                //Spalten hinzufügen.
+                for (int i = 0; i < content[0].Split(';').Length; i++)
+                {
+                    dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                }
+
+                for (int i = 0; i < content.Length; i++)
+                {
+                    dataGridView1.Rows.Add(content[i].Split(';'));
+                } 
+            }
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void chkBxFswEnable_CheckedChanged(object sender, EventArgs e)
+        {
+            fileSystemWatcher.EnableRaisingEvents = chkBxFswEnable.Checked;
+
+            if (chkBxFswEnable.Checked)
+            {
+                chkBxFswEnable.Text = "ONLINE";
+                fileSystemWatcher.Path = file.DirectoryName;
+                fileSystemWatcher.Filter = file.Name; 
+            }
+            else
+            {
+                chkBxFswEnable.Text = "OFFLINE";
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV Files|*.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                file = new FileInfo(ofd.FileName);
+                lblCurrentFile.Text = file.FullName;
+
+                if (file != null)
+                {
+                    UpdateDGV();
+                    chkBxFswEnable.Enabled = true;
+                }
+                else
+                {
+                    chkBxFswEnable.Enabled = false;
+                }
+                
+            }
+            else
+            {
+                chkBxFswEnable.Enabled = false;
+            }
+
+            
+        }
+
+        private void fileSystemWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Thread.Sleep(100);
+            UpdateDGV();
+        }
+    }
+}
